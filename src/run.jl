ec03b4cd1a032df63e00ed14246a64a10722651e
ttekesuke <tekesuke1986@hotmail.com>
2019-03-05 00:59:35 +0900

    position duration velocity取得

--- src/run.jl
+++ src/run.jl
@@ -3,11 +3,17 @@ include("./src/trieMusic.jl")
 ############## midiデータからtrie木を作る ##############
 using MIDI
 #midiファイル読み込み
-midi = MIDI.readMIDIFile("./midi/bach.mid")
+midi = MIDI.readMIDIFile("./midi/bach_2.mid")
 #1トラック目取得
 notes = getnotes(midi.tracks[1], midi.tpq)
 #ピッチ取得
 pitchSequence = [Int(notes[i].pitch) for i in 1:length(notes)]
+
+durationSequence = [Int(notes[i].duration) for i in 1:length(notes)]
+positionSequence = [Int(notes[i].position) for i in 1:length(notes)]
+velocitySequence = [Int(notes[i].velocity) for i in 1:length(notes)]
+
+
 #階差数列に変換
 differenceSequence = trieMusic.seqenceToDifferenceSequence(pitchSequence)
 #2値の数列に変換
@@ -16,16 +22,16 @@ binarySequence = trieMusic.sequenceToBinarySequence(differenceSequence)
 #トライ木の各階層のノードの数、ビット配列の階層を示す区切り位置を取得する
 bitAry, label, eachLevelNodesAccumulatedNumber, levelBoundaryBaIdx = @time trieMusic.sequenceToLouds(binarySequence)
 #全部分列の情報を取得する
-#subSequences, startSeqIdxes, endSeqIdxes = trieMusic.getAllSubSequences(label, pitchSequence, eachLevelNodesAccumulatedNumber)
+subSequences, startSeqIdxes, endSeqIdxes = trieMusic.getAllSubSequences(label, pitchSequence, eachLevelNodesAccumulatedNumber)
 
 #全部分列のDTW距離を計算する
-#println(trieMusic.calcDtwDistances(subSequences))
+println(trieMusic.calcDtwDistances(subSequences))
 
 #ルートノードの直下から指定したノードまでの部分列の形状を持つ、実際の部分列群を取得
-subSequences, startSeqIdxes, endSeqIdxes = trieMusic.getMatchSubsequences(pitchSequence, label, eachLevelNodesAccumulatedNumber, 421)
+# subSequences, startSeqIdxes, endSeqIdxes = trieMusic.getMatchSubsequences(pitchSequence, label, eachLevelNodesAccumulatedNumber, 421)
 
-#取得した部分列群を表示
-trieMusic.plotSubsequences(pitchSequence, subSequences, startSeqIdxes, endSeqIdxes)
+# #取得した部分列群を表示
+# trieMusic.plotSubsequences(pitchSequence, subSequences, startSeqIdxes, endSeqIdxes)
 
 #PlantUMLファイルを生成　長さによっては出力ファイルが大きくなるので注意
-#trieMusic.trieToPuml(bitAry, label)
+trieMusic.trieToPuml(bitAry, label)
